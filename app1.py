713343877f087b4739b227b7606909ac04d88780
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2024-08-16 23:46:15 +0530

    app1.py

--- app1.py
+++ app1.py
@@ -51,7 +51,7 @@ def get_prices():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000,debug=True)
+    app.run('0.0.0.0',debug=True)
 
 # get_price("PRMFBCHJCZJHUEM8")
 
